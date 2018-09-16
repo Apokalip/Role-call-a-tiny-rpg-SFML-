ebfaf598b75efa1d60959413d1eea233c75eb037
Apokalip <43317481+Apokalip@users.noreply.github.com>
2018-09-16 18:01:50 +0300

    Add files via upload

---
+++ Role Call/enemy.cpp
@@ -0,0 +1,47 @@
+#include "main_game.h"
+#include "enemy.h"
+
+enemy::enemy(Map* map, float x, float y)
+{
+	this->Load("enemy.png");
+	this->setPosition(x, y);
+	this->setColor(sf::Color::Red);
+	this->map = map;
+	this->groupId = 5;
+	this->health = 1;
+	this->state = 0;
+}
+
+bool enemy::Update(sf::RenderWindow* window)
+{
+	if (this->health <= 0)
+	{
+		this->Destroy();
+	}
+
+	if (state == 0)
+	{
+		sf::Color seeThrough = this->getColor();
+		seeThrough.a = 255;
+		this->setColor(seeThrough);
+	}
+	else if (state > 0)
+	{
+		state -= 1;
+	}
+
+	Entity::Update(window);
+	return true;
+}
+
+void enemy::Collision(Entity* entity)
+{
+	switch (entity->GroupID())
+	{
+	case 4:
+		entity->Destroy();
+		this->health -= 1;
+		break;
+	}
+}
+
---
+++ Role Call/main_guy.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "entity.h"
+#include "map.h"
+#include "SaveSystem.h"
+
+extern SaveSystem saveSystem;
+
+class main_guy : public Entity
+{
+public:
+	main_guy(EntityManager* entityManager, Map* map, float x, float y);
+	bool Update(sf::RenderWindow* window);
+	void Collision(Entity* entity);
+private:
+	Map* map;
+	float speed;
+	EntityManager* entityManager;
+	bool spaceKey, xKey;
+	float direction;
+};
+
